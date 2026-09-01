bc6f132af9e459f4f3f5b79cef3e189c9766f410
Daniel Schmidt <dsc@charlietango.dk>
2026-09-01 02:45:46 +0200

    Keep fallow off the generated template

--- packages/cli/src/init/scaffold.ts
+++ packages/cli/src/init/scaffold.ts
@@ -2,9 +2,7 @@ import fs from "node:fs"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 
-export const templateRoot = fileURLToPath(
-  new URL("../../template/", import.meta.url)
-)
+const templateRoot = fileURLToPath(new URL("../../template/", import.meta.url))
 
 export type ThemeName = "broadsheet" | "deckard"
 
